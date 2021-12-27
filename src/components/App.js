cadbc3bf6bdec2272a029b0e48b905d3a6529b94
yael hava <yaelhava2610@gmail.com>
2021-12-27 22:44:03 +0200

    add details

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Card, Alert } from 'react-bootstrap'
 import { useAuth } from "../contexts/AuthContext"
 import 'firebase/firestore';
-import { useLocation } from 'react-router-dom';
+import { collection, getDocs, getFirestore } from "firebase/firestore"; 
 
 
 
@@ -10,31 +10,54 @@ export default function Profile() {
     const [error, setError] = useState("");
     const { currentUser } = useAuth();
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-    // const location = useLocation();
-
-
-    // const app = document.getElementById(currentUser.uid);
-    // console.log(location.state);
     
+    var name, email, birth, address;
+
+    async function getDet(){
+
+        const db = getFirestore();
+        const docRef = collection(db, "users");
+        const docSnap = await getDocs(docRef);
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        for (let i = 0; i < 5; i++) {
+            
+            if(currentUser.uid === docSnap.docs[i].id){
+                
+                name = docSnap.docs[i]._document.data.value.mapValue.fields.name.stringValue;
+                birth = docSnap.docs[i]._document.data.value.mapValue.fields.birth.stringValue;
+                address = docSnap.docs[i]._document.data.value.mapValue.fields.address.stringValue;
+                email = docSnap.docs[i]._document.data.value.mapValue.fields.email.stringValue;
+                console.log(name+ birth + address);
+
+            }
+            
+        }
+    
+    }
+
+
     return (
         <>
-            <Card>
-                <Card.Body>
-                <h2 className="text-center mb-4">Profile</h2>
-                {error && <Alert varient="danger">{error}</Alert>}
-                <strong>Email: </strong> {currentUser.email}
-                </Card.Body>
-            </Card>
-           
+        <Card>
+            <Card.Body>
+            <h2 className="text-center mb-4">Profile</h2>
+            {error && <Alert varient="danger">{error}</Alert>}
 
-
+            <button onClick={getDet}>Click to see details</button>
+            <br />
+            {/* <strong>Email: </strong> {currentUser.email} */}
+            
+            <strong>Name:{name} </strong> 
+            <br />
+            <strong>Email: </strong> {email}
+            <br />
+            <strong>Birth Date: </strong> {birth}
+            <br />
+            <strong>Address:</strong> {address}
+            
+            </Card.Body>
+        </Card>
         </>
     )
 }
